3079dc584c2a7600a145d106b32a67573ab199ba
Rafael Rodríguez Martínez <48961409+RafaelRodMar@users.noreply.github.com>
2022-11-30 19:12:00 +0100

    making the game loop

--- ecs-engine/game.h
+++ ecs-engine/game.h
@@ -4,6 +4,7 @@
 #include "SDL_image.h"
 #include <iostream>
 #include <vector>
+#include <list>
 
 class AssetManager;
 class ColliderComponent;
@@ -11,23 +12,39 @@ class ColliderComponent;
 class Game
 {
 public:
-	Game();
+	static Game* Instance()
+	{
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new Game();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
 	~Game();
 
-	void init(const char* title, int width, int height, bool fullscreen);
+	SDL_Renderer* getRenderer() const { return m_pRenderer; }
+
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
 
 	void handleEvents();
 	void update();
 	void render();
 
-	bool running() { return isRunning; }
+	bool running() { return m_bRunning; }
 	void clean();
+	void quit();
 
-	static SDL_Renderer *renderer;
-	static bool isRunning;
+	int getGameWidth() const { return m_gameWidth; }
+	int getGameHeight() const { return m_gameHeight; }
 
 private:
+	Game();
+	static Game* s_pInstance;
+	SDL_Renderer* m_pRenderer;
+	SDL_Window *m_pWindow;
+	bool m_bRunning;
 
-	int cnt = 0;
-	SDL_Window *window;
+	int m_gameWidth;
+	int m_gameHeight;
 };
--- ecs-engine/main.cpp
+++ ecs-engine/main.cpp
@@ -1,39 +1,162 @@
 #include<SDL.h>
+#include<SDL_ttf.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<list>
+#include<cmath>
+#include "game.h"
+
+//la clase juego
+Game* Game::s_pInstance = 0;
+
+Game::Game() {
+	m_pRenderer = NULL;
+	m_pWindow = NULL;
+}
+
+Game::~Game() {
+
+}
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 
-int main(int argc, char* args[])
+bool Game::init(const char* title, int xpos, int ypos, int width,
+	int height, bool fullscreen)
 {
-	// initialize SDL
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	// almacenar el alto y ancho del juego.
+	m_gameWidth = width;
+	m_gameHeight = height;
+
+	// attempt to initialize SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
 	{
-		// if succeeded create our window
-		g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL",
-			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-			640, 480,
-			SDL_WINDOW_SHOWN);
-		// if the window creation succeeded create our renderer
-		if (g_pWindow != 0)
+		int flags = 0;
+		if (fullscreen)
 		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+			flags = SDL_WINDOW_FULLSCREEN;
+		}
+
+		std::cout << "SDL init success\n";
+		// init the window
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos,
+			width, height, flags);
+		if (m_pWindow != 0) // window init success
+		{
+			std::cout << "window creation success\n";
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+			if (m_pRenderer != 0) // renderer init success
+			{
+				std::cout << "renderer creation success\n";
+				SDL_SetRenderDrawColor(m_pRenderer,
+					255, 255, 255, 255);
+			}
+			else
+			{
+				std::cout << "renderer init fail\n";
+				return false; // renderer init fail
+			}
+		}
+		else
+		{
+			std::cout << "window init fail\n";
+			return false; // window init fail
 		}
 	}
 	else
 	{
-		return 1; // sdl could not initialize
+		std::cout << "SDL init fail\n";
+		return false; // SDL init fail
 	}
-	// everything succeeded lets draw the window
-	// set to black // This function expects Red, Green, Blue and
-	// Alpha as color values
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-	// clear the window to black
-	SDL_RenderClear(g_pRenderer);
-	// show the window
-	SDL_RenderPresent(g_pRenderer);
-	// set a delay before quitting
-	SDL_Delay(5000);
-	// clean up SDL
+	if (TTF_Init() == 0)
+	{
+		std::cout << "sdl font initialization success\n";
+	}
+	else
+	{
+		std::cout << "sdl font init fail\n";
+		return false;
+	}
+
+	std::cout << "init success\n";
+	m_bRunning = true; // everything initiated successfully, start the main loop
+
+	return true;
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+	case SDL_QUIT:
+		m_bRunning = false;
+		break;
+	default:
+		break;
+	}
+}
+
+void Game::update()
+{
+}
+
+void Game::render()
+{
+	SDL_RenderClear(m_pRenderer);
+	SDL_RenderPresent(m_pRenderer);
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
 	SDL_Quit();
+}
+
+void Game::quit()
+{
+}
+
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
+int main(int argc, char* args[])
+{
+	Uint32 frameStart, frameTime;
+
+	std::cout << "game init attempt...\n";
+	if (Game::Instance()->init("Let's make games", 100, 100, 600, 400,
+		false))
+	{
+		std::cout << "game init success!\n";
+		while (Game::Instance()->running())
+		{
+			frameStart = SDL_GetTicks(); //tiempo inicial
+
+			Game::Instance()->handleEvents();
+			Game::Instance()->update();
+			Game::Instance()->render();
+
+			frameTime = SDL_GetTicks() - frameStart; //tiempo final - tiempo inicial
+
+			if (frameTime < DELAY_TIME)
+			{
+				//con tiempo fijo el retraso es 1000 / 60 = 16,66
+				//procesar handleEvents, update y render tarda 1, y hay que esperar 15
+				//cout << "frameTime : " << frameTime << "  delay : " << (int)(DELAY_TIME - frameTime) << endl;
+				SDL_Delay((int)(DELAY_TIME - frameTime)); //esperamos hasta completar los 60 fps
+			}
+		}
+	}
+	else
+	{
+		std::cout << "game init failure - " << SDL_GetError() << "\n";
+		return -1;
+	}
+	std::cout << "game closing...\n";
+	Game::Instance()->clean();
+
 	return 0;
 }
